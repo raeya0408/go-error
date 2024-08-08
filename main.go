cd563d2d386444151e97ad00116b3163daf11f5c
raeya0408 <raeya0408@163.com>
2024-08-08 17:22:24 +0800

    001error-21

--- main.go
+++ main.go
@@ -8,14 +8,30 @@ import (
 
 func main() {
 	ints := []int{3, 7, 9, 4, 4}
+
 	//切片int convert string
 	fmt.Printf("%T", convert1(ints))
+	fmt.Println(convert1(ints))
+
 	fmt.Printf("%T", convert2(ints))
+	fmt.Println(convert2(ints))
+
+
+	fmt.Printf("%T", convert22(ints))
+	fmt.Println(convert22(ints))
+
+
 	fmt.Printf("%T", convert3(ints))
+	fmt.Println( convert3(ints))
+
+	fmt.Printf("%T", convert33(ints))
+	fmt.Println( convert33(ints))
+
+
 
 }
 
-// 21实现一个convert方法，将切片Foo转换成另一个类型的切片Bar
+// 21实现一个convert方法，将切片int转换成另一个类型的切片string
 // 法一
 func convert1(ints []int) []string {
 	strs := make([]string, 0)
@@ -42,4 +58,26 @@ func convert3(ints []int) []string {
 	}
 	return strs
 }
+//若需要将一个int切片转换成一个两倍长的string切片，则使用索引复制的方式看起来不清晰，且不易维护
+//若需要将一个Foo切片转换成一个两倍长的Bar切片，则使用索引复制的方式看起来不清晰，且不易维护
+//法二
+func convert22(ints []int)[]string{
+	n:=len(ints)
+	strs:=make([]string,0,2*n)
+	for _,s:=range ints{
+		strs=append(strs, strconv.Itoa(s))
+		strs=append(strs, strconv.Itoa(s))
+	}
+	return strs
+}
+//法三
+func convert33(ints []int)[]string{
+	n := len(ints)
+	strs := make([]string, 2*n)
+	for i, ints := range ints {
+		strs[2*i] = strconv.Itoa(ints)
+		strs[2*i+1] = strconv.Itoa(ints)
 
+	}
+	return strs
+}
