ee05f0da4de96888131ea9c9ed900370aca4a366
raeya0408 <raeya0408@163.com>
2024-08-09 14:04:51 +0800

    001error-22-24

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"fmt"
 	"strconv"
@@ -8,6 +7,9 @@ import (
 
 func main() {
 	err21()
+	err22()
+	err24()
+
 
 
 
@@ -88,3 +90,73 @@ func convert33(ints []int)[]string{
 //22切片为nil和空混淆
 //nil,切片等于nil
 //空，长度为零
+//nil也是空切片，空切片不一定是nil切片
+func err22(){
+	var s1 []string//法一
+	s2:=[]string(nil)//法二
+	s3:=[]string{}//法三
+	s4:=make([]string, 0)//法四
+	
+	fmt.Printf("empty=%t\tnil=%t\n",len(s1)==0,s1==nil)
+	fmt.Printf("empty=%t\tnil=%t\n",len(s2)==0,s2==nil)
+	fmt.Printf("empty=%t\tnil=%t\n",len(s3)==0,s3==nil)
+	fmt.Printf("empty=%t\tnil=%t\n",len(s4)==0,s4==nil)	
+}
+
+
+
+//23没有正确检查切片是否为空
+//切片用法三法四初始化
+//错误写法
+// func handleOperations(id string){
+// 	operations:=getOperation(id)
+// 	if operations!=nil{
+// 		handle(operations)
+// 	}
+// }
+// func getOperation(id string)[]string{
+// 	operation:=make([]string,0)
+// 	if id==""{
+// 		return operation
+// 	}
+// 	return operation
+// }
+//正确写法
+//法一	修改被调用方
+// func getOperation(id string)[]string{
+// 	operation:=make([]string,0)
+// 	if id==""{
+// 		return nil
+// 	}
+// 	return operation
+// }
+//法二	修改调用方
+// func handleOperations(id string){
+// 	operations:=getOperation(id)
+// 	if len(operations)!=0{
+// 		handle(operations)
+// 	}
+// }
+
+
+
+//24错误的切片拷贝
+//错误示例
+// src:=[]int{1,2,3}
+// var dst []int
+// copy(dst,src)//原因在于内置的copy函数，拷贝的切片的元素个数等于min（len（电视台），len（双人床））
+// fmt.Println(dst)
+func err24(){
+	src:=[]int{1,2,3}
+	//法一
+	dst1:=make([]int,len(src))
+	copy(dst1,src)
+	fmt.Println(dst1)
+	//法二
+	dst2:=append([]int(nil),src...)
+	fmt.Println(dst2)
+}
+
+//25切片使用append的副作用
+
+
